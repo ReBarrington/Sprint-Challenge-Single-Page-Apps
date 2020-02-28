e06fd14c95beecac54d3cf1c1bd9ed8a41e87dd5
Reagan Barrington <reagan@barringtonfamily.net>
2020-02-28 13:37:57 -0500

    underline removed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from "react";
 import { Route, Switch, Link } from "react-router-dom";
 import Header from "./components/Header.js";
-import CharacterCard from "./components/CharacterCard";
 import WelcomePage from "./components/WelcomePage";
 import CharacterList from "./components/CharacterList.js";
 import styled from "styled-components";
+
+const StyledLink = styled(Link)`
+    text-decoration: none;
+`;
 
 
 export default function App() {
   return (
     <main>
-      <Link to="/">
+      <StyledLink to="/">
         <Header />
-      </Link>
+      </StyledLink>
       <Switch>
         <Route path="/characters">
           <CharacterList />
         </Route>
         <Route path="/">
-          <WelcomePage />
+          <WelcomePage 
+          className="main-img"
+          src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+          alt="rick"/>
         </Route>
       </Switch>
     </main>
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import "../index.css"
 export default function Header(props) {
   return (
     <header className="ui centered">
-      <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+      <h1 className="ui center">Rick &amp; Morty Fan Page!</h1>
       <img className="main-img" src={props.src}></img>
     </header>
   );
